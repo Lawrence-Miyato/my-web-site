031992e7603ff993e53138e93a373713db3084ca
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2025-07-12 14:19:03 +0700

    Reapply "create home page"
    This reverts commit 0924a731447829208de28c63f21483d4f84446be.

---
+++ src/app/shared/footer.tsx
@@ -0,0 +1,8 @@
+// components/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="text-center py-4 border-t mt-auto text-sm text-gray-500">
+      © {new Date().getFullYear()} Thhuận Website. All rights reserved.
+    </footer>
+  );
+}
